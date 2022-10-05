9dc40b5fff74afa6665d58e7024bee43325e688c
Pimpaphorn Chaichompoo <p.pimpapare@gmail.com>
2022-10-05 13:21:44 +0700

    Initial Commit

---
+++ MovieDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieDemo
+//
+//  Created by Pimpaphorn Chaichompoo on 5/10/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
